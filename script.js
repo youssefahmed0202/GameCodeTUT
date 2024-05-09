342ec96688cb3dbde24331685f813fdd329f41fe
youssefahmed0202 <153360622+youssefahmed0202@users.noreply.github.com>
2024-05-09 16:16:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDXKu6KfZgImif_wY0o4QctWMMT_tuPrnE",
+
+    authDomain: "gamecodetut.firebaseapp.com",
+
+    databaseURL: "https://gamecodetut-default-rtdb.firebaseio.com",
+
+    projectId: "gamecodetut",
+
+    storageBucket: "gamecodetut.appspot.com",
+
+    messagingSenderId: "113985124974",
+
+    appId: "1:113985124974:web:13190982a7ab1099b1847f"
+
+  };
